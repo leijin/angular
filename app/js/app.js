4c3f8f1051530ceceba7a3d99f9d7d0a2f5d3a3d
Jin Lei <leijin@yahoo.com>
2015-01-14 11:20:39 -0800

    ui.bootstarp

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@
 
 /* App Module */
 
-var myapp = angular.module('myapp', ["ui.router"])
+var myapp = angular.module('myapp', ["ui.router", "ui.bootstrap"]);
     myapp.config(function($stateProvider, $urlRouterProvider){
       
       
@@ -34,3 +34,19 @@ var myapp = angular.module('myapp', ["ui.router"])
               }
           });
     });
+
+myapp.controller('CarouselDemoCtrl', function ($scope) {
+  $scope.myInterval = 5000;
+  var slides = $scope.slides = [];
+  $scope.addSlide = function() {
+    var newWidth = 600 + slides.length + 1;
+    slides.push({
+      image: 'http://placekitten.com/' + newWidth + '/300',
+      text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+    });
+  };
+  for (var i=0; i<4; i++) {
+    $scope.addSlide();
+  }
+});
