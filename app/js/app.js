f58247fe73389963c5ad3dab6f2e40ac431da304
Jin Lei <leijin@yahoo.com>
2015-04-21 13:12:31 -0700

    route & footer

--- app/js/app.js
+++ app/js/app.js
@@ -10,27 +10,17 @@ var myapp = angular.module('myapp', ["ui.router", "ui.bootstrap"]);
       $urlRouterProvider.otherwise("/welcome");
       
       $stateProvider
-        .state('route1', {
-          url: "/route1",
-          templateUrl: "partials/route1.html"
+        .state('about', {
+          url: "/about",
+          templateUrl: "partials/about.html"
         })
-        .state('route1.list', {
-          url: "/list",
-          templateUrl: "partials/route1.list.html",
-          controller: function($scope){
-            $scope.items = ["A", "List", "Of", "Items"];
-          }
+        .state('pastors', {
+          url: "/pastors",
+          templateUrl: "partials/pastors.html"
         })
-        .state('route2', {
-          url: "/route2",
-          templateUrl: "partials/route2.html"
-        })
-        .state('route2.list', {
-          url: "/list",
-          templateUrl: "partials/route2.list.html",
-          controller: function($scope){
-            $scope.things = ["A", "Set", "Of", "Things"];
-          }
+        .state('belief', {
+          url: "/belief",
+          templateUrl: "partials/belief.html"
         })
         .state('welcome', {
           url: "/welcome",
@@ -39,8 +29,8 @@ var myapp = angular.module('myapp', ["ui.router", "ui.bootstrap"]);
         });
     });
 
-myapp.controller('CarouselDemoCtrl', function ($scope) {
-  $scope.myInterval = 5000;
+myapp.controller('Carousel', function ($scope) {
+  $scope.slideInterval = 5000;
   var slides = $scope.slides = [];
   $scope.slides.push({
     image: 'img/threesome.png',
@@ -64,57 +54,3 @@ function Ctrl($scope)
 {
     $scope.date = new Date();
 }
-
-/*
-*  AngularJS Directive for Twitter's Embedded Timeline with support for custom CSS.
-*  https://github.com/userapp-io/twitter-timeline-angularjs
-*/
-
-angular.module('twitter.timeline', [])
-  .directive('twitterTimeline', [function() {
-    return {
-      restrict: 'A',
-      scope: {
-        cssUrl: "@",
-        autoResize: "="
-      },
-      link: function (scope, element, attrs) {
-        $('body').removeAttr('data-twttr-rendered');
-
-        element
-          .attr('id', 'twitter-feed')
-          .attr("width", "100%" || attrs.width)
-          .attr('data-chrome', 'noheader transparent')
-          .attr('data-widget-id', attrs.twitterTimeline)
-          .addClass('twitter-timeline');
-
-        function render() {
-          var body = $('.twitter-timeline').contents().find('body');
-
-          if (scope.cssUrl) {
-            body.append($('<link/>', { rel: 'stylesheet', href: scope.cssUrl, type: 'text/css' }));
-          }
-
-          function setHeight() {
-            if (body.find('.stream').length == 0) {
-              setTimeout(setHeight, 100);
-            } else {
-              body.find('.stream').addClass('stream-new').removeClass('stream').css('height', 'auto');
-              $('.twitter-timeline').css('height', (body.height() + 20) + 'px');
-            }
-          }
-
-          if (scope.autoResize) {
-            setHeight();
-          }
-        }
-
-        if (!$('#twitter-wjs').length) {
-          $.getScript((/^http:/.test(document.location)?'http':'https') + '://platform.twitter.com/widgets.js', function() {
-            render();
-            $('.twitter-timeline').load(render);
-              });
-        }
-      }
-    };
-  }]);
